3cf893fc0ecb6d3a16251d4c182ac52621ac7c75
Nacho <nachofernandezavila@gmail.com>
2021-11-08 09:01:01 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Tenemos que mover el vehiculo hacia delante
+        transform.Translate(40 * Time.deltaTime * Vector3.forward);
+        //tarea terminada
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# practica1nacho
+ practica1
